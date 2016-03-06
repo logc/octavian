6c7c8cc7a99df9c527ac4c01f01d32f5d323e686
Luis Osa <luis.osa.gdc@gmail.com>
2016-03-06 14:11:15 +0100

    Add Aitken and Hoerner's methods

--- nonlinear.rkt
+++ nonlinear.rkt
@@ -9,16 +9,13 @@
 ;;   enough to attain the zero.
 (define (bisection f a b 𝛆 [nmax (k_min a b 𝛆)])
   (cond
-    [((f a) . = . 0) a]
-    [((f b) . = . 0) b]
+    [(= (f a) 0) a]
+    [(= (f b) 0) b]
     [else
-      (let ([x ((+ a b) . / . 2)]
-            [I ((- b a) . / . 2)])
+      (let ([x (/ (+ a b) 2)]
+            [I (/ (- b a) 2)])
         (cond
-          [(or (I . < . 𝛆)
-               (nmax . = . 0)
-               ((f x) . = . 0))
-           x]
+          [(or (I . < . 𝛆) (= nmax 0) (= (f x) 0)) x]
           [((* (f a) (f x)) . < . 0)
            (bisection f a x 𝛆 (sub1 nmax))]
           [((* (f x) (f b)) . < . 0)
@@ -33,7 +30,7 @@
 
 ;; LOGARITHM of number `n` in base `base`
 (define (logarithm base n)
-  ((log n) . / . (log base)))
+  (/ (log n) (log base)))
 
 
 (module+ test
@@ -72,12 +69,9 @@
 ;;   derivative function `df` and a starting point `x_0`.  The function also
 ;;   takes a tolerance `𝛆` and a maximum number of iterations `nmax`.
 (define (newton f df x_0 𝛆 nmax)
-  (let* ([diff (* -1 ((f x_0) . / . (df x_0)))]
-         [x (x_0 . + . diff)])
-    (cond [(or ((f x) . = . 0)
-               (nmax . = . 0)
-               ((abs diff) . <= . 𝛆))
-           x]
+  (let* ([diff (/ (f x_0) (df x_0))]
+         [x (- x_0 diff)])
+    (cond [(or (= (f x) 0) (= nmax 0) ((magnitude diff) . <= . 𝛆)) x]
           [else (newton f df x 𝛆 (sub1 nmax))])))
 
 (module+ test
@@ -110,3 +104,45 @@
           [f (curry avg-return-rate 1000 6000)]
           [df (curry davg-return-rate 1000)])
       (check-= (newton f df 0.01 𝛆 3) 0.06140241153618 𝛆))))
+
+;; NEWTONSYS not implemented
+(define (newtonsys) empty)
+
+;; AITKEN finds approximations of fixed point `𝜶` of function `𝜙` starting from
+;;   initial point `x_0` using Aitken's extrapolation method. The method stops
+;;   after a given `nmax` iterations (default: 100), or after the absolute
+;;   value of the difference between two consecutive iterations is less than a
+;;   given tolerance `𝛆` (default: 1 * 10e-4).
+(define (aitken 𝜙 x [𝛆 1e-4] [nmax 100] [niter 0] [diff (add1 𝛆)])
+  (cond
+    [(or (>= niter nmax) (<= diff 𝛆)) x]
+    [else (let* ([gx (𝜙 x)]
+                 [ggx (𝜙 gx)]
+                 [x_new (/ (- (* x ggx) (sqr gx)) (+ ggx (* -2 gx) x))])
+            (aitken 𝜙 x_new 𝛆 nmax (add1 niter) (magnitude (- x x_new))))]))
+
+(module+ test
+  (test-case
+    "Find root 𝜶 = 1 of function `f(x) = e^x * (x - 1)` using Aitken's method"
+    (let ([𝜙_0 (lambda (x) (log (* x (exp x))))]
+          [𝜙_1 (lambda (x) (/ (+ (exp x) x) (+ (exp x) 1)))]
+          [𝛆 1e-10]
+          [x_0 2])
+      (check-= (aitken 𝜙_0 x_0 𝛆) 1.0 𝛆)
+      (check-= (aitken 𝜙_1 x_0 𝛆) 1.0 𝛆))))
+
+;; HOERNER evaluates efficiently polynomial `a` at point `z`. The polynomial
+;;   is expected to be a list of coefficients (a_0 ... a_n), ordered from lowest
+;;   to highest degree.
+(define (hoerner a z)
+  (cond
+    [(empty? a) 0]
+    [else (+ (first a) (* z (hoerner (rest a) z)))]))
+
+(module+ test
+  (test-case
+    "Check that Hörner synthetic division does the same as a normal evaluation"
+    (check-eqv? (hoerner '(1 1 1) 2) (+ 1 (* 1 2) (* 1 (sqr 2))))
+    (check-eqv? (hoerner '(0 2 3 1) 3) (+ 0 (* 2 3) (* 3 (sqr 3)) (expt 3 3)))
+    (check-eqv? (hoerner '(1 1 1) 0) 1)
+    (check-eqv? (hoerner '(7 0 1) 1) (+ 7 (sqr 1)))))
