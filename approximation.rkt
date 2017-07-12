7ca634d5a4803558fd5263e163c9f020d597fc4d
Luis Osa <luis.osa.gdc@gmail.com>
2017-07-12 20:27:33 +0200

    Add Chebyshev nodes for interpolation
    If the target function can be sampled at will, choosing a spacing of the sample
    points using the Chebyshev nodes avoids Runge's phenomenon for polynomial
    fitting.

--- approximation.rkt
+++ approximation.rkt
@@ -1,7 +1,12 @@
 #lang racket
-(require math/matrix)
-(provide polyfit)
+(require math/matrix
+         math/array)
+(provide polyfit
+         polyval
+         chebyshev-nodes)
 
+;; POLYFIT returns the Lagrange interpolation polynomial of grade `n` that fits
+;;   values `ys` at points `xs` with least squared error.
 (define (polyfit xs ys grade)
   (define m (add1 grade))
   (define n (length xs))
@@ -18,3 +23,18 @@
         (for/sum ([i (in-range n)])
           (* (list-ref ys i) (expt (list-ref xs i) row))))))
   (matrix-solve M B))
+
+;; POLYVAL evaluates a polynomial `p` at coordinate `x`.
+(define (polyval p x)
+  (for/sum ([coeff (in-array p)]
+            [exponent (in-range (matrix-num-rows p))])
+    (* coeff (expt x exponent))))
+
+;; CHEBYSHEV-NODES are a distribution of n nodes on an interval [a, b] that
+;;   yield an optimal Lagrange interpolation polynomial. They are the abscissas
+;;   of equispaced nodes on the unit semi-circumference. The Langrage
+;;   polynomial resulting from this Chebyshev sampling is less sensitive to
+;;   Runge phenomena.
+(define (chebyshev-nodes a b n)
+  (define x_c (for/list ([i (in-range 0 (add1 n))]) (-1 . * . (cos ((* pi i) . / . n)))))
+  (for/list ([x x_c]) ((/ (+ a b) 2) . - . (* (/ (b . - . a) 2) x))))
